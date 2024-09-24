1eda8ea1ca4249c7ab2358758dbadedb402c536f
Pedro G. Bello <soy.pedrobello99@gmail.com>
2024-09-24 00:35:39 -0300

    New Prime and Factors functions. Misc.

--- utils.js
+++ utils.js
@@ -1,4 +1,6 @@
-export function capitalize(text) {
+const { BsCloudFog } = require("react-icons/bs");
+
+function capitalize(text) {
 	return text
 		.split(" ")
 		.map((word) => {
@@ -6,3 +8,79 @@ export function capitalize(text) {
 		})
 		.join(" ");
 }
+
+function arraySwap(array) {
+	const firstValue = array[0];
+	const lastValue = array[array.length - 1];
+	array[0] = lastValue;
+	array[array.length - 1] = firstValue;
+	return array;
+}
+
+function addNum(array, num = 1) {
+	const result = [];
+	for (let i = 0; i < array.length; i++) {
+		result.push(array[i] + num);
+	}
+	return result;
+}
+
+function addArrays(array1, array2) {
+	const result = [];
+	for (let i = 0; i < array1.length; i++) {
+		result.push(array1[i] + array2[i]);
+	}
+	return result;
+}
+
+function wordsCount(words) {
+	const result = {};
+	for (let i = 0; i < words.length; i++) {
+		const word = words[i];
+		if (!result[word]) {
+			result[word] = 1;
+		} else {
+			result[word] += 1;
+		}
+	}
+	return result;
+}
+
+/*
+	// const previousWhole = Math.floor(sqrt);
+	// range of whole nums from 0 to previous:
+	// const range = [...Array(previousWhole + 1).keys()];
+	// range of prime nums from 2 to previous:
+	// const primes = filterPrimes(range);
+*/
+
+function isPrime(param) {
+	// Numbers less or equal to 1 are not prime:
+	if (param <= 1) return false;
+	const sqrt = param ** 0.5;
+	for (let i = 2; i <= sqrt; i++) {
+		if (param % i === 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function filterPrimes(param) {
+	return param.filter(isPrime);
+}
+
+function getFactors(param) {
+	const factors = [];
+	for (let i = 2; i <= param; i++) {
+		// If i is a factor of num:
+		while (param % i === 0) {
+			isPrime(i) && factors.push(i);
+			// Divide by i to check further factors:
+			param /= i;
+		}
+	}
+	return factors;
+}
+console.log(getFactors(360));
+console.log(isPrime(31));
--- utils.py
+++ utils.py
@@ -1,4 +1,29 @@
-def insertOnIndex(list, i, value):
-    while len(list) < i:
-        list.append(None)
-    return list.append(value)
+def insertOnIndex(param: list, i: int, value):
+    while len(param) < i:
+        param.append(None)
+    return param.append(value)
+
+def isPrime(param: int):
+    # Numbers less or equal to 1 are not prime:
+    if param <= 1:
+        return False
+    for i in range(2, int(param ** 0.5) + 1):
+        if param % i == 0:
+            return False
+    return True
+
+def filterPrimes(param: list):
+    return [num for num in param if isPrime(num)]
+
+def getFactors(param: list):
+    factors: list = []
+    for i in range(2, param + 1):
+        while param % i == 0:
+            isPrime(i) and factors.append(i)
+            # Divide by i to check further factors:
+            param //= i
+    return factors
+
+print(isPrime(89))
+print(getFactors(88))
+print(filterPrimes(range(100)))
